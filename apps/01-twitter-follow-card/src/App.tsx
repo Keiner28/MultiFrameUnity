c871ae55cf8a814238c3330d6001acc8679c327a
Keiner28 <keinergodinez@gmail.com>
2023-11-15 14:09:20 -0600

    Update README.md with MultiFrameUnity

---
+++ apps/01-twitter-follow-card/src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css'
+import { TwitterFollowCard } from './components/TwitterFollowCard'
+
+const users = [
+  {
+    userName: 'keiner28',
+    name: 'Keiner Godínez',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export default function App() {
+  return (
+    <section className="App">
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  )
+}
