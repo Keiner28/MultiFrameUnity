49026efc45cd522df020520d489a4a3e58d76506
Keiner28 <keinergodinez@gmail.com>
2023-12-22 14:44:18 -0600

    feat(06-shopping-card): :sparkles: react shop with useContext+useReducer

--- apps/06-shopping-cart/src/App.jsx
+++ apps/06-shopping-cart/src/App.jsx
@@ -1,11 +1,24 @@
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 import { Products } from './componets/product/Products'
+import { Header } from './componets/header/Header'
+import { Footer } from './componets/Footer/Footer'
+import { IS_DEVELOPMENT } from './config'
+import { useFilters } from './hooks/useFilters'
+import { Cart } from './componets/cart/Cart'
+import { CartProvider } from './context/cart.jsx'
 
 function App() {
+  const { filterProducts } = useFilters()
+
+  const filteredProducts = filterProducts(initialProducts)
+
   return (
-    <div className="App">
-      <Products products={products} />
-    </div>
+    <CartProvider>
+      <Header />
+      <Cart />
+      <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer />}
+    </CartProvider>
   )
 }
 
